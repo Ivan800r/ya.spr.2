e78cc1b36bfd870e7c7f6a2ab7dff60062fe4bcf
eav <akelizarovav@gmail.com>
2022-08-03 15:49:10 +0300

    Initial commit

---
+++ src/main/java/model/service/Food.java
@@ -0,0 +1,12 @@
+package model.service;
+
+public abstract class Food {
+
+    //количество продукта в килограммах
+    protected int amount;
+    //цена за единицу
+    protected double price;
+    //флаг, который показывает, вегетарианский ли продукт
+    protected boolean isVegetarian;
+
+}
