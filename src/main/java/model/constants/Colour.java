1cf5263cedbb264980e025bb49dc59c992a66346
meridian285 <akelizarovav@gmail.com>
2022-08-07 23:57:29 +0300

    s2

--- src/main/java/model/constants/Colour.java
+++ src/main/java/model/constants/Colour.java
@@ -1,4 +1,6 @@
 package model.constants;
 
 public class Colour {
+    public static final String RED = "RED";
+    public static final String GREEN = "GREEN";
 }
--- src/main/java/model/constants/Discount.java
+++ src/main/java/model/constants/Discount.java
@@ -1,5 +1,7 @@
 package model.constants;
 
 public class Discount {
+    public static final double ZERO = 0;
+    public static final double SIXTY = 60;
 
 }
